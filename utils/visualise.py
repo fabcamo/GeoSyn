ea092a7e47a9f730aca718e25a27ed718365140b
camposmo <fabian.camposmontero@deltares.nl>
2024-09-05 13:59:14 +0200

    New venv

--- utils/visualise.py
+++ utils/visualise.py
@@ -3,24 +3,23 @@
 # ,x,z,IC: with the first one being the index, x being the x coordinate, z being the z coordinate and IC being the intensity value
 # It also have such a header { ,x,z,IC }
 
-# The function will return a pandas dataframe with the data
-
 import pandas as pd
 from pathlib import Path
-
+import matplotlib.pyplot as plt
 
 # define the path to the csv file
-cs_file = Path(r"C:/Users/camposmo/OneDrive - Stichting Deltares/Documents/Projects/schemaGAN/forRoeland\simple\cs_830.csv")
+cs_file = Path(r"C:/Users/camposmo/OneDrive - Stichting Deltares/Documents/Projects/schemaGAN/forRoeland/simple/cs_830.csv")
 
 # read the data from the csv file
 cs_data = pd.read_csv(cs_file)
+# print the first 5 rows of the data
 print(cs_data.head())
 
 # Rearrange into a table of x no of columns and z no of rows with IC as the values
-cs_data_other = cs_data.pivot(index='z', columns='x', values='IC')
+cs_data_arr = cs_data.pivot(index='z', columns='x', values='IC')
 print(cs_data.head())
 
 # plot the data with imshow
-import matplotlib.pyplot as plt
-plt.imshow(cs_data_other)
+
+plt.imshow(cs_data_arr)
 plt.show()
